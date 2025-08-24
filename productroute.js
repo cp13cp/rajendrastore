1e9e09c1c1e8a084db3cadb4de89abb0d271ac88
yashkawale <kawaleyash001@gmail.com>
2025-08-24 23:56:09 +0530

    Added update and delete routes

--- productroute.js
+++ productroute.js
@@ -44,4 +44,33 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// edit product
+router.put("/products/:id", async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// delete product
+router.delete("/products/:id", async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 export default router;
